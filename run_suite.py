8d4db489b9559daf9cbd1ce50cab346567f91c28
kuroneko-nj <66053093+kuroneko-nj@users.noreply.github.com>
2020-06-11 10:47:49 +0800

    Update run_suite.py

--- run_suite.py
+++ run_suite.py
@@ -6,3 +6,4 @@ import app
 suite = unittest.TestLoader().discover(app.BASE_PATH+'/script',pattern="test*.py")
 file_name = "/report{}.html".format(time.strftime('%Y%m%d-%H%M%S'))
 BeautifulReport(suite).report(filename=file_name,description='TPSHOP注册接口测试',log_path='./report')
+print("*"*10+"测试完成"+"*"*10)
